3bbbdbe138baa98981e435a68619f1eb904ef8fc
sidharthaprusty <siddharth.prusty@gmail.com>
2018-11-21 14:27:25 +0530

    initial Commit

---
+++ Resume Builder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Resume_Builder.Startup))]
+namespace Resume_Builder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
